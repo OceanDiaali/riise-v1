cbbee4c62352299dd7f31923fc359c9e74185ac3
OceanDiaali <ocean.diaali@gmail.com>
2019-03-22 17:34:00 +0100

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//rb2d.velocity = new Vector2 (Input.GetAxisRaw("Horizontal") * speed, 0);
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		Vector2 move = new Vector2 ((moveHorizontal) * speed, 0);
+		rb2d.velocity = move;
+	}
+}
